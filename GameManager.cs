670e9d7b7de5027d2e502ddd762e285cbcb0f9e4
Michael Batista <149213942+MBatista18@users.noreply.github.com>
2023-11-01 18:00:47 -0400

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject enemyOnePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CreateEnemyOne", 1.0f, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void CreateEnemyOne()
+    {
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-8, 8), 7, 0), Quaternion.identity);
+    }
+}
